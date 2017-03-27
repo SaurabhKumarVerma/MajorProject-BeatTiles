74000a6eeff2bfa5d5428732ee138712907af0f6
SaurabhKumarVerma <saurav88871kumar@hotmail.com>
2017-03-27 16:25:23 -0700

    splah activity updated
    slpah activity made

--- Beattiles/app/src/main/java/trainedge/beattiles/HomeActivity.java
+++ Beattiles/app/src/main/java/trainedge/beattiles/HomeActivity.java
@@ -1,11 +1,9 @@
 package trainedge.beattiles;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
+import android.sax.StartElementListener;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -13,17 +11,19 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Thread mythread =new Thread(){
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-        });
+        };
+        mythread.start();
     }
-
 }
