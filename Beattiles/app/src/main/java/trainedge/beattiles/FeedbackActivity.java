2582e0cd4aea3d91194bbaddb9577b3bd0d1347b
SaurabhKumarVerma <saurav88871kumar@hotmail.com>
2017-04-04 15:42:48 -0700

    rating
    updated

--- Beattiles/app/src/main/java/trainedge/beattiles/FeedbackActivity.java
+++ Beattiles/app/src/main/java/trainedge/beattiles/FeedbackActivity.java
@@ -8,11 +8,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-<<<<<<< HEAD
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
-=======
->>>>>>> origin/master
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -21,20 +19,16 @@ import java.util.HashMap;
 
 public class FeedbackActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText etname;
+    private ImageButton imbtn_nolike;
+    private ImageButton imbtn_sad;
+    private ImageButton imbtn_dis;
+    private ImageButton imbtn_epl;
+    private ImageButton imbtn_hapy;
+    private TextView tvfeedbck;
     private EditText etemail;
-    private EditText etcomment;
-    private Button btnsend;
+    private Button sendbtn;
     private FirebaseDatabase db;
-    private DatabaseReference commentRef;
-<<<<<<< HEAD
-    private ImageButton sadbtn;
-    private ImageButton unlikebtn;
-    private ImageButton disaptbtn;
-    private ImageButton explessbtn;
-    private ImageButton happybtn;
-=======
->>>>>>> origin/master
+    private DatabaseReference commentsRfs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,82 +37,56 @@ public class FeedbackActivity extends AppCompatActivity implements View.OnClickL
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-<<<<<<< HEAD
-        sadbtn = (ImageButton) findViewById(R.id.sadbtn);
-        unlikebtn = (ImageButton) findViewById(R.id.unlikebtn);
-        disaptbtn = (ImageButton) findViewById(R.id.disaptbtn);
-        explessbtn = (ImageButton) findViewById(R.id.explessbtn);
-        happybtn = (ImageButton) findViewById(R.id.happybtn);
-
-=======
-        etname = (EditText) findViewById(R.id.etname);
->>>>>>> origin/master
+        imbtn_nolike = (ImageButton) findViewById(R.id.imbtn_nolike);
+        imbtn_sad = (ImageButton) findViewById(R.id.imbtn_sad);
+        imbtn_dis = (ImageButton) findViewById(R.id.imbtn_dis);
+        imbtn_epl = (ImageButton) findViewById(R.id.imbtn_epl);
+        imbtn_hapy = (ImageButton) findViewById(R.id.imbtn_hapy);
+        tvfeedbck = (TextView) findViewById(R.id.tvfeedbck);
         etemail = (EditText) findViewById(R.id.etemail);
-        etcomment = (EditText) findViewById(R.id.etcomment);
-        btnsend = (Button) findViewById(R.id.btnsend);
+        sendbtn = (Button) findViewById(R.id.sendbtn);
+
         db = FirebaseDatabase.getInstance();
-        commentRef = db.getReference("comments");
-        btnsend.setOnClickListener(this) ;
+        commentsRfs = db.getReference("COMMENTS");
+        sendbtn.setOnClickListener(this);
     }
+
     @Override
-    public void onClick(View v){
-        String name =etname.getText().toString();
-        String emailid = etemail.getText().toString();
-        String comment = etcomment.getText().toString();
-<<<<<<< HEAD
-        if (v.getId()==R.id.sadbtn){
+    public void onClick(View v) {
+        String email=etemail.getText().toString();
+        String commentd=tvfeedbck.getText().toString();
 
-            Toast.makeText(this, "1", Toast.LENGTH_SHORT).show();
-             }
-          if(v.getId()==R.id.unlikebtn){
+        if (email.isEmpty()){
 
-              Toast.makeText(this, "2", Toast.LENGTH_SHORT).show();
+            etemail.setError("Fill Your Email");
+            return;
         }
-        if(v.getId()==R.id.disaptbtn){
+        if (commentd.isEmpty()){
 
+            tvfeedbck.setError("Fill Your Comments");
+            return;
+        }
+        HashMap<String,String> map=new HashMap<>();
+        map.put("msg",commentd);
+        map.put("User",email);
+        commentsRfs.push().setValue(map);
+        etemail.setText("");
+        tvfeedbck.setText("");
+
+        if(v.getId()==R.id.imbtn_nolike){
+            Toast.makeText(this, "1", Toast.LENGTH_SHORT).show();
+        }
+        if (v.getId()==R.id.imbtn_sad){
+            Toast.makeText(this, "2", Toast.LENGTH_SHORT).show();
+        }
+        if (v.getId()==R.id.imbtn_dis){
             Toast.makeText(this, "3", Toast.LENGTH_SHORT).show();
         }
-        if (v.getId()==R.id.explessbtn){
-
+        if (v.getId()==R.id.imbtn_epl){
             Toast.makeText(this, "4", Toast.LENGTH_SHORT).show();
         }
-        if (v.getId()==R.id.happybtn){
-
+        if (v.getId()==R.id.imbtn_hapy){
             Toast.makeText(this, "5", Toast.LENGTH_SHORT).show();
         }
-
-=======
->>>>>>> origin/master
-        if (name.isEmpty()) {
-            etname.setError("Please enter your name");
-            return;
-        }
-        if (emailid.isEmpty()) {
-            etemail.setError("Please enter your email");
-            return;
-        }
-        if (comment.isEmpty()) {
-            etcomment.setError("Please add your comment for better experience");
-            return;
-        }
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
-        //firebase upload
-        HashMap<String,String> map=new HashMap<>();
-        map.put("msg", comment);
-        map.put("user", name);
-        map.put("user", emailid);
-
-        commentRef.push().setValue(map);
-        etname.setText("");
-        etemail.setText("");
-        etcomment.setText("");
-
-        }
-
-
     }
-
-
+}
