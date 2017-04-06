6954a40d4995345b521157403ae4d4c0e6379eba
SaurabhKumarVerma <saurav88871kumar@hotmail.com>
2017-04-05 21:22:49 -0700

    updated
    animation on splash screen updated

--- Beattiles/app/src/main/java/trainedge/beattiles/HomeActivity.java
+++ Beattiles/app/src/main/java/trainedge/beattiles/HomeActivity.java
@@ -7,29 +7,56 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private ImageView ivanim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Thread mythread=new Thread(){
-            @Override
-            public void run() {
-                try {
-                    sleep(3000);
-                    Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
-                    startActivity(intent);
-                    finish();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
+        ivanim = (ImageView) findViewById(R.id.ivanim);
 
-        mythread.start();
+        Animation fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        ivanim.setAnimation(fadeInAnimation );
+
+        fadeInAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Thread mythread=new Thread(){
+                    @Override
+                    public void run() {
+                        try {
+                            sleep(3000);
+                            Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                mythread.start();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+
 
     }
 
