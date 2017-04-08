545ede24c85bb321a5299a233d9878740eeb43e2
SaurabhKumarVerma <saurav88871kumar@hotmail.com>
2017-04-08 12:16:40 -0700

    changes on android
    fulll

--- Beattiles/app/src/main/java/trainedge/beattiles/HomeActivity.java
+++ Beattiles/app/src/main/java/trainedge/beattiles/HomeActivity.java
@@ -1,5 +1,6 @@
 package trainedge.beattiles;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -19,6 +20,15 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
 
         ivanim = (ImageView) findViewById(R.id.ivanim);
 
