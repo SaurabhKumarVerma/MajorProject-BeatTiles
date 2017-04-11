bec13accabd21f503f65409936e10bc189108606
Samadhan Singh <ishusingh9721073220@gmail.com>
2017-04-11 15:35:48 +0530

    picasso update
    circular transformation

--- Beattiles/app/src/main/java/trainedge/beattiles/AccountSettingActivity.java
+++ Beattiles/app/src/main/java/trainedge/beattiles/AccountSettingActivity.java
@@ -1,6 +1,10 @@
 package trainedge.beattiles;
 
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -19,6 +23,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserInfo;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
 
 public class AccountSettingActivity extends AppCompatActivity {
 
@@ -56,7 +61,7 @@ public class AccountSettingActivity extends AppCompatActivity {
                 username.setText(name);
                 Picasso.with(this)
                         .load(photoUrl)
-                        .resize(50, 50)
+                        .resize(80, 80)
                         .centerCrop()
                         .into(ivpicaso);
             }
@@ -65,6 +70,40 @@ public class AccountSettingActivity extends AppCompatActivity {
 
         }
 
+        class CircleTransform implements Transformation {
+            @Override
+            public Bitmap transform(Bitmap source) {
+                int size = Math.min(source.getWidth(), source.getHeight());
+
+                int x = (source.getWidth() - size) / 2;
+                int y = (source.getHeight() - size) / 2;
+
+                Bitmap squaredBitmap = Bitmap.createBitmap(source, x, y, size, size);
+                if (squaredBitmap != source) {
+                    source.recycle();
+                }
+
+                Bitmap bitmap = Bitmap.createBitmap(size, size, source.getConfig());
+
+                Canvas canvas = new Canvas(bitmap);
+                Paint paint = new Paint();
+                BitmapShader shader = new BitmapShader(squaredBitmap,
+                        BitmapShader.TileMode.CLAMP, BitmapShader.TileMode.CLAMP);
+                paint.setShader(shader);
+                paint.setAntiAlias(true);
+
+                float r = size / 2f;
+                canvas.drawCircle(r, r, r, paint);
+
+                squaredBitmap.recycle();
+                return bitmap;
+            }
+
+            @Override
+            public String key() {
+                return "circle";
+            }
+        }
 
     }
 
