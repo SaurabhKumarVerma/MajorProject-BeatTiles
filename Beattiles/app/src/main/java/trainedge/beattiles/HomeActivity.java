5877a20b5be4567eb1726481c351027039d0993e
SaurabhKumarVerma <saurav88871kumar@hotmail.com>
2017-04-23 17:12:27 +0530

    Changes
    Designe

--- Beattiles/app/src/main/java/trainedge/beattiles/HomeActivity.java
+++ Beattiles/app/src/main/java/trainedge/beattiles/HomeActivity.java
@@ -3,6 +3,7 @@ package trainedge.beattiles;
 import android.*;
 import android.Manifest;
 import android.app.ActionBar;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
@@ -14,12 +15,16 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+import static com.facebook.FacebookSdk.getApplicationContext;
+
+public class HomeActivity extends Activity {
 
     private static final int REQUEST_STORAGE = 332;
     private ImageView ivanim;
@@ -27,9 +32,13 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_home);
 
-        requestUser();
+
         ivanim = (ImageView) findViewById(R.id.ivanim);
 
         Animation fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fadein);
@@ -69,22 +78,8 @@ public class HomeActivity extends AppCompatActivity {
 
     }
 
-    private void requestUser() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_STORAGE);
-            }
-            return;
-        }
-    }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_STORAGE) {
-            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "please give permission", Toast.LENGTH_SHORT).show();
-                requestUser();
-            }
+
+
         }
-    }
-}
+
--- Beattiles/app/src/main/java/trainedge/beattiles/SliderActivity.java
+++ Beattiles/app/src/main/java/trainedge/beattiles/SliderActivity.java
@@ -4,12 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -39,6 +35,7 @@ public class SliderActivity extends Activity {
 
 
         setContentView(R.layout.activity_slider);
+
 
 
         viewPager = (ViewPager) findViewById(R.id.view_pager);
